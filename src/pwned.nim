f5c4f05f1684e78f29380b8f42fb6860f1cd6ecf
Sultan Al Isaiee <sultan@foxoman.net>
2022-06-19 20:55:49 +0400

    Update pwned.nim
    - Use std/sha1
    - Use puppy to fetch the api
    - Use termui to build TUI.
    - Use scanf to extract data from api result instead of split/regex ..etc

--- src/pwned.nim
+++ src/pwned.nim
@@ -1,9 +1,6 @@
-import securehash, httpclient, strutils, strformat
+import std/[sha1,strscans], strutils, puppy, termui
 
-import utils
-
-const
-  apiUrl = "https://api.pwnedpasswords.com/range/"
+const apiUrl = "https://api.pwnedpasswords.com/range/"
 
 proc rangeCheck(hash: SecureHash): int =
   ## Takes a SHA1 hash and returns the corresponding password's occurrences
@@ -13,22 +10,44 @@ proc rangeCheck(hash: SecureHash): int =
   let prefix = ($hash)[0..<5]
   let suffix = ($hash)[5..^1]
 
-  let client = newHttpClient()
-  let response = client.get(apiUrl & prefix)
-  if response.code == Http200:
-    for line in response.body.splitLines():
-      let parts = line.split(":")
-      assert parts.len == 2
+  let response = Request(url: parseUrl(apiUrl & prefix), verb: "get").fetch()
 
-      if suffix == parts[0]:
-        return parts[1].parseInt()
+  var sufx: string
+  var occ: int
+
+  if response.code == 200:
+    for line in response.body.splitLines():
+      discard scanf(line, "$+:$i", sufx, occ)
+      if suffix == sufx:
+        return occ
+  elif response.code == 429:
+    stderr.writeLine "[*] Too many requests — the rate limit has been exceeded."
+  else:
+    stderr.writeLine "[*] Unknown Error."
 
 when isMainModule:
-  stdout.write("Please enter your password: ")
-  let password = getPassword()
-  if password.len == 0:
-    quit("No password entered.")
+  echo """
+           __          __ __    __      __ __
+          |__)|  ||\ ||_ |  \  |__) /\ (_ (_
+          |   |/\|| \||__|__/  |   /--\__)__)
+                                v: 1.0 @foxoman
 
-  let hash = secureHash(password)
-  let occurrences = rangeCheck(hash)
-  echo(fmt"Your password was found to be used {occurrences} times")
+  A command line utility that lets you check if a passphrase has been
+  pwned using the Pwned Passwords v2 API.
+
+  All provided password data is k-anonymized before sending to the API,
+  so plaintext passwords never leave your computer.
+
+  ** See: https://haveibeenpwned.com/Passwords
+  """
+  var password = termuiAskPassword("Please enter a passphrase to check if has been pwned:")
+  if password.len == 0:
+    stderr.writeLine "[*] No passphrase entered."
+  else:
+    let hash = secureHash(password)
+    let occurrences = rangeCheck(hash)
+    if occurrences == 0:
+      termuiLabel("Wow, Your passphrase look secure,", "NOT Pwned!")
+    else:
+      termuiLabel("Oh no -- Pwned! Your passphrase was found to be used:", "$1 times!" % [$occurrences])
+      termuiLabel("[**WARN**]", "This password has previously appeared in a data breach and should never be used. If you've ever used it anywhere before, change it!")
